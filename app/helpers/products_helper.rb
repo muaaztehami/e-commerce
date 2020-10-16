0d68bdace04fe4e640c7c73b6cb592a9e57b1967
muaaztehami <muaaztehami169@gmail.com>
2020-10-16 16:45:57 +0500

    Added: Paperclip

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
   def product_params
-    params.require(:product).permit(:name, :description, :price, :quantity)
+    params.require(:product).permit(:name, :description, :price, :quantity, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_063410) do
+ActiveRecord::Schema.define(version: 2020_10_16_104051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2020_10_16_063410) do
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
