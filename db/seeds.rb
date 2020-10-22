5f19ddcfadda8973fde7be1e342e4e7a159b7a9d
muaaztehami <muaaztehami169@gmail.com>
2020-10-22 13:10:18 +0500

    Updated: seed for ActiveAdmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email:  ENV['E_COMMERCE_ADMIN_EMAIL'] , 
+                  password: ENV['E_COMMERCE_ADMIN_PASSWORD'], 
+                  password_confirmation: ENV['E_COMMERCE_ADMIN_PASSWORD']) if Rails.env.production?
